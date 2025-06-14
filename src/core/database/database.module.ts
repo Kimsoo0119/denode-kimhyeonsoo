c1a75c84e68cf00665530bc039db95a86cfe10ff
kimsoo0119 <illppang@naver.com>
2025-06-14 20:21:33 +0900

    Init: 프로젝트 기본 세팅 완료

---
+++ src/core/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/../../entities/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') === 'development',
+        logging: configService.get('NODE_ENV') === 'development',
+        namingStrategy: new SnakeNamingStrategy(),
+        timezone: 'Z',
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
